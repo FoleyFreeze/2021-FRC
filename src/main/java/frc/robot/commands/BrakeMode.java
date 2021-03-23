61c2d845c591faa125d84a0968c5df9041e68dfa
Roboprog <roboprog@BishopFoley.org>
2021-03-23 19:27:50 -0400

    added paths, bounce path command

---
+++ src/main/java/frc/robot/commands/BrakeMode.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+
+public class BrakeMode extends CommandBase{
+
+    RobotContainer m_subsystem;
+    boolean on;
+
+    public BrakeMode(RobotContainer subsystem, boolean on){
+        m_subsystem = subsystem;
+        addRequirements(m_subsystem.m_drivetrain);
+        this.on = on;
+    }
+
+    @Override
+    public void initialize(){
+        m_subsystem.m_drivetrain.setBrake(on);
+    }
+
+    @Override
+    public void execute() {
+        
+    }
+
+    @Override
+    public void end(boolean interrupted){
+
+    }
+
+    @Override
+    public boolean isFinished(){
+        return on == m_subsystem.m_drivetrain.getBrake();
+    }
+}
